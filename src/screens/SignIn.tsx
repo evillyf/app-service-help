34bb3dad0cdd4391e581ac9a8fd04a9ab932e65a
Evilly <61359076+evillyf@users.noreply.github.com>
2022-07-19 17:08:17 -0300

    SignIn
    tela de login com logo, inputs de e-mail e senha e botão de entrar.

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,48 @@
+import { Heading, VStack, Icon, useTheme } from 'native-base';
+import Logo from '../assets/logo_primary.svg';
+import { Input } from '../components/input';
+import { Envelope, Key } from 'phosphor-react-native';
+import { Button } from '../components/Button';
+import { useState } from 'react';
+
+
+
+export function SignIn(){
+
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const { colors } = useTheme();
+
+    function handleSignIn() {
+        console.log(name, password);
+    }
+
+    return(   
+        <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+            <Logo />
+
+            <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+                Acesse sua conta 
+            </Heading>
+
+            <Input 
+            placeholder="E-mail"
+            mb={4}
+            InputLeftElement={<Icon as={<Envelope color={colors.gray[100]} />} ml={4}/>}
+            onChangeText={setName}
+
+            />
+            <Input 
+            mb={8}
+            placeholder="Senha"
+            InputLeftElement={<Icon as={<Key color={colors.gray[100]} />} ml={4}/>}
+            secureTextEntry
+            onChangeText={setPassword}
+            />
+
+            <Button title = "Entrar" w="full" onPress={handleSignIn}/>
+
+        </VStack>
+
+    )
+}
